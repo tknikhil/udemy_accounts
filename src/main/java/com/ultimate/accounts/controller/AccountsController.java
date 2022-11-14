07d8d299735fa6cade8e91bb92cd8aeab976d68b
Nikhil TK <nikhiltk1114@gmail.com>
2022-11-14 13:02:30 +0530

    added correlation id cross cutting concern concept

--- src/main/java/com/ultimate/accounts/controller/AccountsController.java
+++ src/main/java/com/ultimate/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -23,6 +24,8 @@ import com.ultimate.accounts.service.client.CardsFeignClient;
 import com.ultimate.accounts.service.client.LoansFeignClient;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class AccountsController {
@@ -59,10 +62,11 @@ public class AccountsController {
 	
 	@PostMapping("/myCustomerDetails")
 	@CircuitBreaker(name="detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
-	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+	@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
+	public CustomerDetails myCustomerDetails(@RequestHeader("eazybank-correlation-id")String correlationId, @RequestBody Customer customer) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
-		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+		List<Loans> loans = loansFeignClient.getLoansDetails(correlationId,customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(correlationId,customer);
 
 		CustomerDetails customerDetails = new CustomerDetails();
 		customerDetails.setAccounts(accounts);
@@ -73,14 +77,25 @@ public class AccountsController {
 
 	}
 	
-	private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+	private CustomerDetails myCustomerDetailsFallBack(@RequestHeader("eazybank-correlation-id")String correlationId,Customer customer, Throwable t) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Loans> loans = loansFeignClient.getLoansDetails(correlationId,customer);
 		CustomerDetails customerDetails = new CustomerDetails();
 		customerDetails.setAccounts(accounts);
 		customerDetails.setLoans(loans);
 		return customerDetails;
 
 	}
+	
+	@GetMapping("/sayHello")
+	@RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+	public String sayHello() {
+		return "Hello, Welcome to EazyBank";
+	}
+
+	private String sayHelloFallback(Throwable t) {
+		return "Hi, Welcome to EazyBank";
+	}
+
 
 }
